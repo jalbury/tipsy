7e66e274209eee9e2e4ffe0dba0fa7c674c53ac9
Locodays <jon.a.havener@knights.ucf.edu>
2019-03-04 15:57:42 -0500

    This is the water physics

---
+++ Tipsy/Assets/Scripts/CollidesWithThreshold.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollidesWithThreshold : MonoBehaviour {
+    public Rigidbody Prefab;
+    int count = 0;
+
+    public void OnParticleCollision(GameObject other)
+    {
+        count++;
+        print(count);
+        SpawnLiquid theScript = other.GetComponent<SpawnLiquid>();
+
+        if(other.CompareTag("isCupThreshold"))
+        {
+            print("Collided with threshold");
+            if (theScript != null)
+            {
+                theScript.spawnObject(Prefab);
+            }
+            else
+            {
+                print("Script is null");
+            }
+
+        }
+        else
+        {
+            print(other.tag);
+        }
+
+            
+    }
+
+}
---
+++ Tipsy/Assets/Scripts/SpawnLiquid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLiquid : MonoBehaviour {
+    
+    public Transform Spawnpoint;
+    //public Rigidbody Prefab;
+    int count = 0;
+
+
+    public void spawnObject(Rigidbody Prefab)
+    {
+        count++;
+        print("Instantiation :"+count);
+
+        Rigidbody RigidPrefab;
+
+        var radius = (float)Random.Range(0, 100)/1000;
+        var rotation = Random.Range(0, 360);
+
+        Vector3 spot = new Vector3((Spawnpoint.position.x+radius*Mathf.Cos(rotation)),
+                        Spawnpoint.position.y,
+                        (Spawnpoint.position.z+radius*Mathf.Sin(rotation)));
+        
+
+        RigidPrefab = Instantiate(Prefab, spot,
+                    Spawnpoint.rotation) as Rigidbody;
+        RigidPrefab.transform.parent = gameObject.transform;
+    }  
+
+}
