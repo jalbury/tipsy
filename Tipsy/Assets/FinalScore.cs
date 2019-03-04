be3664bc30140b05215c3b92e028132e4ddae3c9
MrMatt7 <matthewB0217@gmail.com>
2019-03-04 11:38:52 -0500

    Added Score Scene with the associated Script, and End Game Buttons

---
+++ Tipsy/Assets/FinalScore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalScore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		this.gameObject.GetComponent<TextMesh>().text = "Score: " + DataManager.getScore().ToString();
+	}
+}
---
+++ Tipsy/Assets/StartMenuButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuButton : MonoBehaviour {
+
+	public Material defaultMat;
+	public Material hoverMat;
+	public Material clickMat;
+
+	public void onHover()
+	{
+		this.GetComponent<MeshRenderer>().material = hoverMat;
+	}
+
+	public void onClick()
+	{
+		this.GetComponent<MeshRenderer>().material = clickMat;
+
+        // Loads Main Menu Scene
+		SceneManager.LoadScene("StartMenu");
+	}
+}
+
