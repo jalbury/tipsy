eef9efd65cb1c0efa266128e40fe0f9e973ebaa9
MrMatt7 <matthewB0217@gmail.com>
2019-03-20 12:43:40 -0400

    Added Oculus Input scripts with functionality to interact with buttons

---
+++ Tipsy/Assets/Scripts/Pointer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    public float defaultLength = 5.0f;
+    public GameObject dot;
+    public OculusInputManager inputManager;
+
+    private LineRenderer lineRenderer = null;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        UpdateLine();
+    }
+
+    private void UpdateLine()
+    {
+        // Use Default or new Distance
+        float targetLength = defaultLength;
+
+        // Raycast
+        RaycastHit hit = CreateRaycast(targetLength);
+
+        // Default
+        Vector3 endPosition = transform.position + (transform.forward * targetLength);
+
+        // Or Based off of hit
+        if(hit.collider != null)
+            endPosition = hit.point;
+
+        // Set Position of Dot
+        dot.transform.position = endPosition;
+
+        // Set LineRenderer
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, endPosition);
+
+    }
+
+    private RaycastHit CreateRaycast(float length)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.Raycast(ray, out hit, length);
+
+        return hit;
+    }
+}
