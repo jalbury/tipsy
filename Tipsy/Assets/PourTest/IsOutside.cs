023c9dceb7e9ae9a7ae2262835ccf370881c7b23
Locodays <jon.a.havener@knights.ucf.edu>
2019-03-25 19:45:36 -0400

    Created bounding for objects

---
+++ Tipsy/Assets/PourTest/IsOutside.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsOutside : MonoBehaviour {
+
+    public GameObject comparisonPoint;
+
+    void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject == other.transform.parent.Find("OVRCameraRig").GetComponent<RaycastPointer>().pickedUpObject)
+            other.transform.parent.Find("OVRCameraRig").GetComponent<RaycastPointer>().objDistance -= (float)0.1;
+        //other = (Collider)objectWithin.GetComponent(typeof(MeshFilter));
+        /*
+        Vector3 direction = comparisonPoint.transform.position - other.transform.position;
+        direction.Normalize();
+        direction.x *= (float).1;
+        direction.y *= (float).1;
+        direction.z *= (float).1;
+        other.transform.position += direction;
+        */
+    }
+
+
+}
---
+++ Tipsy/Assets/PourTest/IsWithin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsWithin : MonoBehaviour {
+
+    public GameObject comparisonPoint;
+
+
+   void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == other.transform.parent.Find("OVRCameraRig").GetComponent<RaycastPointer>().pickedUpObject)
+            other.transform.parent.Find("OVRCameraRig").GetComponent<RaycastPointer>().objDistance += (float) 0.1;
+
+        /*
+        Vector3 direction = comparisonPoint.transform.position - other.transform.position;
+        direction.Normalize();
+        direction.x *= (float).1;
+        direction.y *= (float).1;
+        direction.z *= (float).1;
+        other.transform.position -= direction;
+        */
+    }
+
+
+}
