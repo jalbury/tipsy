bf08d89535a5f9c991baf3e7452b3ac4be7c0681
jalbury <jalbury@knights.ucf.edu>
2019-03-29 20:17:45 -0400

    added customer navigation

---
+++ Tipsy/Assets/Scripts/MoveAgent.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveAgent : MonoBehaviour
+{
+    public Transform destination;
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        SetDestination();
+    }
+
+    void SetDestination()
+    {
+        agent.SetDestination(destination.transform.position);
+    }
+
+    //private void Update()
+    //{
+    //    if (Arrived())
+    //        animator.SetBool("stopWalking", true);
+    //}
+
+    //private bool Arrived()
+    //{
+    //    return (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance
+    //        && (!agent.hasPath || agent.velocity.sqrMagnitude == 0f));
+    //}
+}
