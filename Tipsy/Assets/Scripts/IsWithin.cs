48c58c9a57151ad95f9fefe106d6606dc1c3c23f
Locodays <jon.a.havener@knights.ucf.edu>
2019-03-29 21:54:21 -0400

    Added bounding box to main scene

--- Tipsy/Assets/Scripts/IsWithin.cs
+++ Tipsy/Assets/Scripts/IsWithin.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class IsWithin : MonoBehaviour {
 
-    public GameObject comparisonPoint;
     public GameObject cameraRig;
-
 
    void OnTriggerStay(Collider other)
     {
@@ -14,16 +12,5 @@ public class IsWithin : MonoBehaviour {
         {
             cameraRig.GetComponent<RaycastPointer>().objDistance += (float)0.1;
         }
-
-
-        /*
-        Vector3 direction = comparisonPoint.transform.position - other.transform.position;
-        direction.Normalize();
-        direction.x *= (float).1;
-        direction.y *= (float).1;
-        direction.z *= (float).1;
-        other.transform.position -= direction;
-        */
     }
-
 }
---
+++ Tipsy/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour {
+
+    Animator animator;
+    public bool stopWalking = false;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        print("hello");
+        StartCoroutine(PlayAnimation());
+    }
+
+    IEnumerator PlayAnimation()
+    {
+        yield return new WaitForSeconds(3);
+        print("walking");
+        animator.Play("Walk");
+        yield return new WaitForSeconds(3);
+        print("sitting");
+        animator.Play("SitDown");
+        animator.Play("SittingHandsOnLap");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
