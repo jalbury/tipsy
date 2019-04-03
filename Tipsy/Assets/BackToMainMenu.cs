78c6c3d28d9c08e4c832c7e1ecf0cb1dad0aa5a3
MrMatt7 <matthewB0217@gmail.com>
2019-04-02 21:20:19 -0400

    Added Level Selection Scene

---
+++ Tipsy/Assets/BackToMainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMainMenu : MonoBehaviour
+{
+	public Material defaultMat;
+	public Material hoverMat;
+	public Material clickMat;
+
+	public void onHover()
+	{
+		this.GetComponent<MeshRenderer>().material = hoverMat;
+	}
+
+	public void onDefault()
+	{
+		this.GetComponent<MeshRenderer>().material = defaultMat;
+	}
+	public void onClick()
+	{
+		this.GetComponent<MeshRenderer>().material = clickMat;
+		SceneManager.LoadScene("StartMenu");	
+	}
+}
---
+++ Tipsy/Assets/Level3Button.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level3Button : MonoBehaviour
+{
+	public Material defaultMat;
+	public Material hoverMat;
+	public Material clickMat;
+
+	public void onHover()
+	{
+		this.GetComponent<MeshRenderer>().material = hoverMat;
+	}
+
+	public void onDefault()
+	{
+		this.GetComponent<MeshRenderer>().material = defaultMat;
+	}
+	public void onClick()
+	{
+		this.GetComponent<MeshRenderer>().material = clickMat;
+		SceneManager.LoadScene("Level_3_Name");	
+	}
+}
