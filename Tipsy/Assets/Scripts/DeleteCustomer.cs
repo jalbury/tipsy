b738fb2d0684c4ad60c4047643618d670369ef3b
jalbury <jalbury@knights.ucf.edu>
2019-04-28 20:58:27 -0400

    fix bug in DeleteCustomer.cs

--- Tipsy/Assets/Scripts/DeleteCustomer.cs
+++ Tipsy/Assets/Scripts/DeleteCustomer.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DeleteCustomer : MonoBehaviour {
+    HashSet<GameObject> customers;
+
+    private void Start()
+    {
+        customers = new HashSet<GameObject>();
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(other.gameObject);
+        // don't destroy customers the first time they enter trigger; destroy the
+        // second time (when they're leaving bar)
+        GameObject customer = other.gameObject;
+        if (customers.Contains(customer))
+            Destroy(customer);
+        else
+            customers.Add(customer);
     }
 }
